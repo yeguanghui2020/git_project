8a005daae99c24b79550511f1e2abf8cf353eab5
yeguanghui2020 <74913323+yeguanghui2020@users.noreply.github.com>
2020-11-30 21:48:37 +0800

    Update StudentModel.java

--- student_prc/src/com/student/model/StudentModel.java
+++ student_prc/src/com/student/model/StudentModel.java
@@ -2,11 +2,11 @@ package com.student.model;
 
 public class StudentModel {
 
-	private int id;
-	private String username;
-	private String password;
-	private int sex;
-	private String love;
+	private int id; //ID
+	private String username; //用户
+	private String password; //密码
+	private int sex; //性别
+	private String love; //爱好
 	
 	public StudentModel(int id, String username, String password, int sex, String love) {
 		super();
